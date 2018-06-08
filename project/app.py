ad113f5ec08bf9f0480ea63364d06e801872c720
DAOS-Computer-Science <daoscompsci@gmail.com>
2018-06-08 22:28:44 +0100

    tidy up time...
    Just tidied it up a bit

--- project/app.py
+++ project/app.py
@@ -2,13 +2,15 @@
 
 import pandas as pd
 from flask import Flask, url_for, redirect, render_template, request
+
 app = Flask(__name__)
 
-#Universal function
+
+# Universal function
 def room(csvfile, homepage):
     df = pd.read_csv(csvfile)
     if request.method == 'POST':
-        #gets all user inputs and converts to variables
+        # gets all user inputs and converts to variables
         title = request.form['title']
         firstname = request.form['firstName']
         surname = request.form['surname']
@@ -16,48 +18,48 @@ def room(csvfile, homepage):
         daychosen = request.form['dayChosen']
         timechosen = request.form['timeChosen']
         reason = request.form['reason']
-        
-        #converts variables to respective number in database
+
+        # converts variables to respective number in database
         days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
         daychosen = days.index(daychosen) + 1
         times = ['Registration', 'P1', 'P2', 'P3', 'Lunch', 'P4', 'P5', 'AfterSchool']
-        timechosen = times.index(timechosen) 
-          
-        #converts every cell to a string
-        df = df.applymap(str) 
-        
-        if df.iat[timechosen, daychosen] != "Free" :
-          return render_template('failure.html', currentroom = roomnum, message = "Cell is already taken!")
-        
+        timechosen = times.index(timechosen)
+
+        # converts every cell to a string
+        df = df.applymap(str)
+
+        if df.iat[timechosen, daychosen] != "Free":
+            return render_template('failure.html', currentroom=roomnum, message="Cell is already taken!")
 
         # puts data together as a variable
-        #celldata = title + " " + firstname + " " + surname + "\n" + reason
+        # celldata = title + " " + firstname + " " + surname + "\n" + reason
         celldata = title + " " + firstname + " " + surname + " (" + reason + ")"
         # adds the cell data to the relevant cell
         df.iat[timechosen, daychosen] = celldata
-        
-        #writes to the csv
-        df.to_csv(csvfile, index = False)
-        
-        return render_template('success.html', currentroom = roomnum)
-      
+
+        # writes to the csv
+        df.to_csv(csvfile, index=False)
+
+        return render_template('success.html', currentroom=roomnum)
+
     else:
-        
-        #converts dataframe to array and outputs array to webpage
-        df = df.drop(df.columns[0], axis = 1)
+
+        # converts dataframe to array and outputs array to webpage
+        df = df.drop(df.columns[0], axis=1)
         df = df.values
-        
-        
-        return render_template(homepage, df = df)
-    
+
+        return render_template(homepage, df=df)
+
 
 @app.route('/')
 def home():
     return redirect(url_for('K6'))
-                    
+
+
 @app.route('/K6', methods=['POST', 'GET'])
 def K6():
     return room('databases/currentweekK6.csv', 'homeK6.html')
+
 
 @app.route('/K5', methods=['POST', 'GET'])
 def K5():
