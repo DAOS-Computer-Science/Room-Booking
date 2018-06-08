c196799c32bf938a5b61b4787323dcc7d3072683
daoscompsci <daoscompsci@gmail.com>
2018-06-08 14:21:55 -0400

    Created folder subtasks, and added tasks that need working on for the backend

---
+++ project/subtasks/app_delete.py
@@ -0,0 +1,15 @@
+#PROBLEMSET:
+#Given the following inputs (title, firstname, surname etc.), convert the cell associated with
+#these inputs to "Free".
+
+#variables that the website gets from the user (You can change them for testing purposes)
+title = "Mr"
+firstname = "Rushin"
+surname = "Shah"
+roomnum = "K6"
+daychosen = "Monday"
+timechosen = "Registration"
+reason = "Test reason"
+
+# Write your code here
+         
