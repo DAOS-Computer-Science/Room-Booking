3168297225115137776741554617a7e1134f7a92
RoyPhysics <roy.physics999@gmail.com>
2019-01-19 13:24:53 +0000

    Starting Documentation

--- project/login- ALPHA/login.js
+++ project/login- ALPHA/login.js
@@ -1,10 +1,14 @@
-function allow() {}
-function login() {
+var allow = function(title,forename,surname) {
+document.getElementsByName('title').selectedIndex = title;
+document.getElementsByName('firstName').value = forename;
+document.getElementsByName('surname').value = surname;
+}
+
+var login = function() {
   var name = document.getElementById('name');
   var password = document.getElementById('password');
-  if (name === "A Person") {
-    if (password ==='pass') {
-      allow('A person')
-    }
+
+  if (name === "A Person" && password ==='pass') {
+    allow('Mr','A','Person');
   }
 }
